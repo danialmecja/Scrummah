1e0e2cb7b7383ae8f5c5ebfa09899f766f56871d
Danial Mecja <danialmecja@gmail.com>
2024-01-23 23:29:15 +0800

    Added circle, speaker no.s and padding

--- Scrummah/MeetingView.swift
+++ Scrummah/MeetingView.swift
@@ -11,7 +11,31 @@ struct MeetingView: View {
     var body: some View {
         VStack {
             ProgressView(value: 10, total: 15)
+            HStack {
+                
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed").font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining").font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+                
+            }
+            
+            Circle()
+                .strokeBorder(lineWidth: 21.9)
+            HStack{
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Image(systemName: "forward.fill")
+                }
+            }
         }
+        .padding()
     }
 }
     
