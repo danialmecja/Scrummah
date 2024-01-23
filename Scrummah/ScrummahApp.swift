0c3d315cf50b9069d3f3bcfde2b4b4f33f5f9753
Danial Mecja <danialmecja@gmail.com>
2024-01-23 22:48:09 +0800

    Initial Commit

---
+++ Scrummah/ScrummahApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrummahApp.swift
+//  Scrummah
+//
+//  Created by Danial Mecja on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrummahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
